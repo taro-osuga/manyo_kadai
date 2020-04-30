16dfb63f47c9392dc3736414a7d5391f200850b3
taroosuga <copernix0@gmail.com>
2020-04-30 13:31:15 +0900

    DBにnull: falseを追加

--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,8 @@ ActiveRecord::Schema.define(version: 2020_04_27_043939) do
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
+    t.string "title", null: false
+    t.text "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
