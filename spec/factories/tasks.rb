d5e3c70efb90771fe0444445cb927ede25f4f32a
taroosuga <copernix0@gmail.com>
2020-04-30 13:21:58 +0900

    タスク降順テスト実装

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,6 +1,10 @@
 FactoryBot.define do
   factory :task do
-    title { 'test_title' }
-    content { 'test_content' }  
+    title { 'Factoryで作ったデフォルトのタイトル１' }
+    content { 'Factoryで作ったデフォルトのコンテント1' }
+  end
+  factory :second_task, class: Task do
+    title { 'Factoryで作ったデフォルトのタイトル2' }
+    content { 'Factoryで作ったデフォルトのコンテント2' }
   end
 end
