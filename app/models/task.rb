9dab331677807ee9c7abfbcbb614e27e9e24cfbf
taroosuga <copernix0@gmail.com>
2020-05-12 19:25:42 +0900

    ラベル機能実装

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,10 +5,14 @@ class Task < ApplicationRecord
 
     scope :search_title, -> (title) { where("title LIKE ?", "%#{ title }%") }
     scope :search_status, -> (status) { where(status: status) }
+    scope :search_labels, -> (task_id) { where(id: [task_id]) }
 
     enum priority:{
         高: 0, #高い
         中: 1, #普通
         下: 2, #低い
       }
+
+    has_many :task_labels, dependent: :destroy, foreign_key: 'task_id'
+    has_many :labels, through: :task_labels, source: :label
 end
