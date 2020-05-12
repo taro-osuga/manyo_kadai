c52595611e815c600c7496aacb50b36e707550ca
taroosuga <copernix0@gmail.com>
2020-05-12 21:19:09 +0900

    css修正

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,6 +1,6 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe Task, type: :model do
-  FactoryBot.create(:user)
-  FactoryBot.create(:task, user: user)
-end
+# RSpec.describe Task, type: :model do
+#   FactoryBot.create(:user)
+#   FactoryBot.create(:task, user: user)
+# end
--- spec/system/search_spec.rb
+++ spec/system/search_spec.rb
@@ -1,14 +1,19 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
   before do
-    FactoryBot.create(:task)
-    FactoryBot.create(:second_task)
-    FactoryBot.create(:third_task)
+    user = FactoryBot.create(:user)
+    FactoryBot.create(:task, user: user)
+    FactoryBot.create(:second_task, user: user)
+    FactoryBot.create(:third_task, user: user)
   end
 
     describe 'タスク一覧画面' do
       context '検索をした場合' do
         it "タイトルで検索できる" do
+          visit new_session_path
+          fill_in 'Email', with: 'sample@example.com'
+          fill_in 'Password', with: '00000000'
+          click_on 'Log in'       
           visit tasks_path
           fill_in "タイトル検索", with: 'タイトル１'
           click_on '検索'
@@ -17,6 +22,10 @@ RSpec.describe 'タスク管理機能', type: :system do
       end
       context '検索をした場合' do
         it "ステータスで検索できる" do
+          visit new_session_path
+          fill_in 'Email', with: 'sample@example.com'
+          fill_in 'Password', with: '00000000'
+          click_on 'Log in'       
           visit tasks_path
           select '完了', from: 'status'
           click_button '検索'
@@ -25,6 +34,10 @@ RSpec.describe 'タスク管理機能', type: :system do
       end
       context '検索をした場合' do
         it "タイトルとステータスで検索できる" do
+          visit new_session_path
+          fill_in 'Email', with: 'sample@example.com'
+          fill_in 'Password', with: '00000000'
+          click_on 'Log in'       
           visit tasks_path
           fill_in "タイトル検索", with: 'タイトル3'
           select '着手中', from: 'status'
@@ -34,6 +47,10 @@ RSpec.describe 'タスク管理機能', type: :system do
       end
       context '終了期限でソートをした場合', :retry => 3 do
         it "並び替えできる" do
+          visit new_session_path
+          fill_in 'Email', with: 'sample@example.com'
+          fill_in 'Password', with: '00000000'
+          click_on 'Log in'       
           visit tasks_path
           click_on '終了期限でソートする'
           task_list = all('tbody tr') 
@@ -45,6 +62,10 @@ RSpec.describe 'タスク管理機能', type: :system do
     
       context '重要度でソートをした場合', :retry => 3 do
         it "並び替えできる" do
+          visit new_session_path
+          fill_in 'Email', with: 'sample@example.com'
+          fill_in 'Password', with: '00000000'
+          click_on 'Log in'       
           visit tasks_path
           click_link '優先順位でソートする'
           task_list = all('tbody tr') 
